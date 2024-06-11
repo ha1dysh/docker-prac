ac8712fe2c961798510810b44e26b14f00aaa3a3
ha1dysh <olekshaid@gmail.com>
2024-06-11 22:50:39 +0300

    docker-compose

--- create_user.sh
+++
@@ -1,2 +0,0 @@
-groupadd -g $APP_GROUP $APP_GROUPNAME
-useradd -m -s /bin/bash -u $APP_USER -g $APP_GROUP $APP_USERNAME
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-groupadd -g $APP_GROUP $APP_GROUPNAME || echo "exist"
-useradd -m -s /bin/bash -u $APP_USER -g $APP_GROUP $APP_USERNAME || echo "exist"
-
-exec "$@"
